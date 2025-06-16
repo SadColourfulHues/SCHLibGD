4b60addbd0f347be079bcb57c3027a3453d0bcd2
SadColourfulHues <sadcolourfulhues@gmail.com>
2025-06-16 23:13:38 +0800

    (feat) added Repeater SCHLib/Misc 	-> added 'shutdown' signal to VFXSprite2D

--- 2D/vfx_sprite_2d.gd
+++ 2D/vfx_sprite_2d.gd
@@ -3,6 +3,9 @@
 class_name VFXSprite2D
 extends Sprite2D
 
+## Called when the VFX item is about to be destroyed
+signal shutdown()
+
 @export
 var m_duration: float = 0.33
 
@@ -30,6 +33,11 @@ func _ready() -> void:
     tween.tween_property(self, ^"frame", hframes - 1, m_duration)
     tween.tween_callback(func(): self_modulate.a = 0.0)
     tween.tween_interval(m_destruction_delay)
-    tween.tween_callback(queue_free)
+    tween.tween_callback(_on_shutdown)
+
+
+func _on_shutdown() -> void:
+    shutdown.emit()
+    queue_free()
 
 #endregion
---
+++ Misc/repeater.gd
@@ -0,0 +1,60 @@
+## A special utility node for repeatedly performing a specified action
+class_name Repeater
+extends Node
+
+signal elapsed()
+
+@export
+var m_destroy_on_finish := false
+
+var p_timer: Timer
+var p_tmp_callback: Callable
+
+var m_times_left := 0
+
+
+#region Functions
+
+## Starts the repeater
+## (A temporary callback can provided if signals are a bit inconvenient)
+func start(duration: float,
+           count: int = 3,
+           callback := Callable()) -> void:
+
+    p_timer.start(duration)
+    m_times_left = count
+    p_tmp_callback = callback
+
+
+## Stops the repeater
+func stop() -> void:
+    p_timer.stop()
+
+#endregion
+
+#region Events
+
+func _ready() -> void:
+    p_timer = Timer.new()
+    p_timer.timeout.connect(_on_timer_elapsed)
+    add_child(p_timer)
+
+
+func _on_timer_elapsed() -> void:
+    m_times_left -= 1
+    elapsed.emit()
+
+    if p_tmp_callback.is_valid():
+        p_tmp_callback.call()
+
+    if m_times_left > 0:
+        return
+
+    if !m_destroy_on_finish:
+        p_tmp_callback = Callable()
+        p_timer.stop()
+        return
+
+    queue_free()
+
+#endregion
